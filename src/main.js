121ea47dbea6240b71e25c916431ddbdc50d1e94
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-19 19:21:20 +0300

    add directive

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import ColorDirective from './color'
 
 // export const eventEmitter = new Vue()
+Vue.directive('colored',ColorDirective)
 
 new Vue({
   el: '#app',
