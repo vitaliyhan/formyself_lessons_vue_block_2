414f163efdb812d032a64082c1704d3376070eec
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-20 14:56:12 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
-    <h2 v-colored:background="'red'" >{{ title }}</h2>
-    <h2 v-colored:color.border.big="'blue'" >{{ title }}</h2>
+    <h2>{{ title }}</h2>
   </div>
 </template>
 
 <script>
-import ColorDirective from './color'
 export default {
   data() {
     return {
       title: "Hi title!",
     }
   },
-  directives: {
-    colored: ColorDirective
-  }
 }
 </script>
 
 <style scoped>
+
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import App from './App'
-import ColorDirective from './color'
-
-// export const eventEmitter = new Vue()
-// Vue.directive('colored',ColorDirective)
 
 new Vue({
   el: '#app',
