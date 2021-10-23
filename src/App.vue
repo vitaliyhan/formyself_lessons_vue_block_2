64e7c0cbf8c96af3830e6e890cd489d83e45338b
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-23 17:18:08 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,17 @@
 <template>
   <div>
 
-    <h2>From input</h2>
-    <app-on-off v-model="switched"></app-on-off>
-
-
-    <div>
-      <h3 v-if="switched">Component is enabled</h3>
-      <h3 v-else>Component is disabled</h3>
-    </div>
-
-    <my-button v-model="switched"></my-button>
   </div>
 </template>
 
 <script>
-import OnOff from "./OnOff";
-import Button from "./button";
 
 export default {
   data() {
     return {
-      switched: false
+
     }
   },
-  components: {
-    appOnOff: OnOff,
-    myButton: Button
-  }
 }
 </script>
 
