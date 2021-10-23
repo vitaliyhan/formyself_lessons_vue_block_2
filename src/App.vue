48cf2d02d52b581026a8b7c6ba6e0d93dfe4d344
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-23 18:26:20 +0300

    validate mail

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,19 @@
           v-model="email"
           type="email"
           id="email"
+          @blur="$v.email.$touch()"
           class="form-control">
       </div>
+      <pre>
+        {{ $v.email }}
+      </pre>
     </form>
 
   </div>
 </template>
 
 <script>
+import {required, email} from 'vuelidate/lib/validators'
 
 export default {
   data() {
@@ -22,6 +27,12 @@ export default {
       email: ''
     }
   },
+  validations: {
+    email: {
+      required,
+      email
+    }
+  }
 }
 </script>
 
