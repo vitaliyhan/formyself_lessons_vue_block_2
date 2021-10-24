408176e892b9443db7fdb248b6d0767a21af88a6
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 15:44:01 +0300

    routes redirect and 404

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import Cars from './pages/Cars'
 import Car from './pages/Car'
 import CarFull from './pages/CarFull'
+import er404 from './pages/404'
 
 export default new VueRouter({
   routes: [
     {
       path: '',
       component: Home,
+      name: 'home'
     },
     {
       path: '/cars',
@@ -25,6 +27,10 @@ export default new VueRouter({
         }
       ]
     },
+    {
+      path: '/*',
+      component: er404
+    }
   ],
   mode: 'history',
   scrollBehavior(to, from, save) {
