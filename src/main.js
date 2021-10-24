461239ed8131db82ec73655eb58e58cb8568c590
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 20:57:09 +0300

    interseptors

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import App from './App'
 Vue.use(VueResource)
 Vue.http.options.root = 'http://localhost:3000/'
 
+Vue.http.interceptors.push((request, next) => {
+  request.headers.set('Authorization', 'Bearer eyJ0e.....etc')
+  request.headers.set('Accept', 'application/json')
+  next()
+})
+
 new Vue({
   el: '#app',
   render: h => h(App),
