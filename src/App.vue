f7f04e52570312e4ad1fbf4c1cf447af0ca702cc
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 21:17:35 +0300

    init vuex

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container pt-2">
-    <form>
-      <div class="form-group mb-3">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" v-model.trim="carName" id="name">
-      </div>
-      <div class="form-group mb-3">
-        <label for="year">Year</label>
-        <input type="text" class="form-control" v-model.number="carYear" id="year">
-      </div>
-      <button class="btn btn-success" @click.prevent="createCar">createCar</button>
-      <button class="btn btn-primary" @click.prevent="loadCars">Load cars</button>
-    </form>
-    <hr>
-    <ul class="list-group">
-      <li
-        class="list-group-item"
-        v-for="car of cars"
-        :key="car.id"
-      >{{ car.id }} <strong>{{ car.name }}</strong> - {{ car.year }}
-        <button type="button" class="btn btn-outline-danger" @click.prevent="deleteCar(car.id)">
-          <i class="bi bi-trash"></i>
-          Delete
-        </button>
-      </li>
-    </ul>
+
   </div>
 </template>
 
@@ -33,32 +9,11 @@
 export default {
   data() {
     return {
-      carName: 'Name',
-      carYear: 123,
-      cars: [],
-      resource: null
+
     }
   },
   methods: {
-    createCar() {
-      const car = {
-        name: this.carName,
-        year: this.carYear,
-      }
-      this.resource.save({}, car).then(() => this.loadCars())
-    },
-    loadCars() {
-      this.resource.get().then(response => response.json()).then(data => this.cars = data)
-    },
-    deleteCar(id) {
-      this.resource.delete({id: id})
-        .then(
-          () => this.loadCars()
-        )
-    },
-  },
-  created() {
-    this.resource = this.$resource('cars{/id}')
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App'
-
-
-Vue.use(VueResource)
-Vue.http.options.root = 'http://localhost:3000/'
-
-Vue.http.interceptors.push((request, next) => {
-  request.headers.set('Authorization', 'Bearer eyJ0e.....etc')
-  request.headers.set('Accept', 'application/json')
-  next()
-})
 
 new Vue({
   el: '#app',
