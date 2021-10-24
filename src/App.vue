44246db21a12577771f899cd5021b861f276fe04
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 21:52:33 +0300

    vuex getter

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="container text-center pt-5">
     <app-counter></app-counter>
+    <hr>
     <app-actions></app-actions>
+    <hr>
+    <app-second-counter></app-second-counter>
   </div>
 </template>
 
 <script>
 import Counter from './Counter'
+import SecondCounter from './SecondCounter'
 import Actions from './Action'
 
 export default {
   methods: {},
   components: {
     appCounter: Counter,
-    appActions: Actions
+    appActions: Actions,
+    appSecondCounter: SecondCounter,
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,10 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     counter: 0
+  },
+  getters: {
+    computedCounter(state) {
+      return state.counter * 10
+    }
   }
 })
