096d710ae0b29843e84f58221a4c1b1d1e40eaee
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 21:37:20 +0300

    init vuex

---
+++ src/Action.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button class="btn btn-success m-1" @click="updateCounter(1)"><i class="bi bi-plus-circle"></i></button>
+    <button class="btn btn-danger m-1" @click="updateCounter(-1)"><i class="bi bi-dash-circle"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    updateCounter(val) {
+      this.$emit('counterUpdated', val)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="container pt-2">
-
+  <div class="container text-center pt-5">
+    <app-counter :counter="counter"></app-counter>
+    <app-actions @counterUpdated="counter+=$event"></app-actions>
   </div>
 </template>
 
 <script>
-
+import Counter from './Counter'
+import Actions from './Action'
 export default {
   data() {
     return {
-
+      counter: 0
     }
   },
-  methods: {
-
+  methods: {},
+  components: {
+    appCounter: Counter,
+    appActions: Actions
   }
 }
 </script>
