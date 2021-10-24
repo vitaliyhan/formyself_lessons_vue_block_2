6a969b0667fdae76c4ed35de59904b656c27f4ee
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 13:36:30 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,14 @@
 <template>
-  <div class="container">
-    <form class="pt-4" @submit.prevent="onSubmit">
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input
-          v-model="email"
-          type="email"
-          id="email"
-          @blur="$v.email.$touch()"
-          :class="{'is-invalid':$v.email.$error}"
-          class="form-control">
-        <div class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>
-        <div class="invalid-feedback" v-if="!$v.email.email">Email field needs to be email</div>
-        <div class="invalid-feedback" v-if="!$v.email.uniqueEmail">This email already existed</div>
-      </div>
-      <div class="form-group">
-        <label for="password">password</label>
-        <input
-          v-model="password"
-          type="password"
-          id="password"
-          @blur="$v.password.$touch()"
-          :class="{'is-invalid':$v.password.$error}"
-          class="form-control">
-        <div class="invalid-feedback" v-if="!$v.password.minLength">Required min {{ $v.password.$params.minLength.min }}
-          chars
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="confirmPassword">Confirm password</label>
-        <input
-          v-model="confirmPassword"
-          type="password"
-          id="confirmPassword"
-          @blur="$v.confirmPassword.$touch()"
-          :class="{'is-invalid':$v.confirmPassword.$error}"
-          class="form-control">
-        <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">Not the same
-        </div>
-      </div>
-      <button class="btn btn-success mt-3" type="submit" :disabled="$v.$invalid">Submit</button>
-      <pre>{{ $v.$invalid }}</pre>
-    </form>
 
-  </div>
 </template>
 
 <script>
-import {required, email, minLength, sameAs} from 'vuelidate/lib/validators'
 
 export default {
-  methods: {
-    onSubmit() {
-      console.log('Email',this.email)
-      console.log('password',this.password)
-    }
-  },
   data() {
     return {
-      email: '',
-      password: '',
-      confirmPassword: '',
     }
   },
-  validations: {
-    email: {
-      required,
-      email,
-      uniqueEmail: function (newEmail) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            const value = newEmail !== 'test@ya.ru'
-            resolve(true)
-          }, 500)
-        })
-      }
-    },
-    password: {
-      minLength: minLength(8),
-    },
-    confirmPassword: {
-      sameAs: sameAs('password')
-    }
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-import Vuelidate from 'vuelidate'
 import App from './App'
 
-Vue.use(Vuelidate)
 
 new Vue({
   el: '#app',
