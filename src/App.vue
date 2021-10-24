672e9f23180cc36b871b7f6db1b188b0aeb4ee04
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 14:13:08 +0300

    spa working

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+
+        <a class="navbar-brand" href="#">Brand</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            <router-link class="nav-link" to="/cars">Cars</router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
