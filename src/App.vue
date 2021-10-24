b45d12dfc5d012b924637b6f6c55ea0654c705b9
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 20:02:37 +0300

    create get request

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,18 @@
         <label for="year">Year</label>
         <input type="text" class="form-control" v-model.number="carYear" id="year">
       </div>
-
       <button class="btn btn-success" @click.prevent="createCar">createCar</button>
+      <button class="btn btn-primary" @click.prevent="loadCars">Load cars</button>
     </form>
-
+    <hr>
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="car of cars"
+        :key="car.id"
+      >{{ car.id }} <strong>{{ car.name }}</strong> - {{ car.year }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,7 +30,8 @@ export default {
   data() {
     return {
       carName: 'Name',
-      carYear: 123
+      carYear: 123,
+      cars: []
     }
   },
   methods: {
@@ -32,13 +41,22 @@ export default {
         year: this.carYear,
       }
       this.$http.post('http://localhost:3000/cars', car)
-      .then(response=>{
-       return response.json()
-      })
-      .then(newCar =>{
-        console.log(newCar)
-      } )
+        .then(response => {
+          return response.json()
+        })
+        .then(newCar => {
+        })
+    },
+    loadCars() {
+      this.$http.get('http://localhost:3000/cars')
+        .then(response => {
+          return response.json()
+        })
+        .then(cars => {
+          this.cars=cars
+        })
     }
+
   }
 }
 </script>
