8eebf80faa5c76fb03b99b9fbdcf72eded21ae65
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-24 14:03:23 +0300

    initial routes home and car working

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App'
+import router from "./routes"
 
 Vue.use(VueRouter)
 
 new Vue({
   el: '#app',
   render: h => h(App),
+  router
 })
 
 
